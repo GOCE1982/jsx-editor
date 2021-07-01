b34b23693f78acb5da83e88e489e511477667e63
GOCE1982 <goce.ilievski@live.com>
2021-07-02 01:04:19 +0200

    most recent final default cells

--- packages/local-api/src/routes/cells.ts
+++ packages/local-api/src/routes/cells.ts
@@ -26,53 +26,53 @@ export const createCellsRouter = (filename: string, dir: string) => {
         let defaultCells = [
           {
             content:
-              '# JSX Editor \
-              This is an interactive coding environment in which you can write Javascript, see it executed, and write comprehensive documentation and markdown. \
-              - Click any text cell (including this one) to edit it \
-              - The code in each code editor is all joined together in one file. If you define a variable in code cell #1 you can refer to it in any following cell \
-              - *You can show any React component, string, number, or anything else by calling the `show` function. This is a function that is built into the environment. Call show multiple times to show multiple values* \
-              - Re-order or delete cells using the buttons on the top right corner \
-              - Add new cells by hovering on the divider between each cell \
+              '# JSX Editor n\
+              This is an interactive coding environment in which you can write Javascript, see it executed, and write comprehensive documentation and markdown. n\
+              - Click any text cell (including this one) to edit it n\
+              - The code in each code editor is all joined together in one file. If you define a variable in code cell #1 you can refer to it in any following cell n\
+              - *You can show any React component, string, number, or anything else by calling the `show` function. This is a function that is built into the environment. Call show multiple times to show multiple values* n\
+              - Re-order or delete cells using the buttons on the top right corner n\
+              - Add new cells by hovering on the divider between each cell n\
               All of your changes are saved to a file you opened your JSX Editor with. If you run `npx jsx-editor serve test.js`, all of the text and code cells will be saved to the `test.js` file.',
             type: 'text',
             id: 'j1b5k',
           },
           {
             content:
-              "import { useState } from 'react'; \
-\
-            const Counter = () => { \
-              const [count, setCount] = useState(0) \
-              return ( \
-                <div> \
-                  <button onClick={() => setCount(count + 1)}>Click</button> \
-                  <h3>Count: {count}</h3> \
-                </div> \
-              ) \
-            }; \
-            \
-            // Display any variable or React Component by calling `show` \
-            show(<Counter />) \
+              "import { useState } from 'react'; n\
+n\
+            const Counter = () => { n\
+              const [count, setCount] = useState(0) n\
+              return ( n\
+                <div> n\
+                  <button onClick={() => setCount(count + 1)}>Click</button> n\
+                  <h3>Count: {count}</h3> n\
+                </div> n\
+              ) n\
+            }; n\
+            n\
+            // Display any variable or React Component by calling `show` n\
+            show(<Counter />) n\
             ",
             type: 'code',
             id: 'kkgj1',
           },
           {
             content:
-              'const App = () => { \
-              return ( \
-                <div> \
-                  <h2>Hi from the JSX Editor</h2> \
-                  <i>Counter component will be rendered below</i> \
-                  <hr/> \
-                  { \
-                    /* Counter was declared in a previous cell and can be referenced here */ \
-                  } \
-                  <Counter /> \
-                </div> \
-              ) \
-            } \
-            \
+              'const App = () => { n\
+              return ( n\
+                <div> n\
+                  <h2>Hi from the JSX Editor</h2> n\
+                  <i>Counter component will be rendered below</i> n\
+                  <hr/> n\
+                  { n\
+                    /* Counter was declared in a previous cell and can be referenced here */ n\
+                  } n\
+                  <Counter /> n\
+                </div> n\
+              ) n\
+            } n\
+            n\
             show(<App />)',
             type: 'code',
             id: 'jsb4k',
